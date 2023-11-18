b08e42f3ca90e2c5004b3aff7ec05ede736938c8
Crowdbotics <team@crowdbotics.com>
2023-11-18 01:47:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spinai-44654",
+  url: "https://spinai-44654.botics.co",
+  api: "https://spinai-44654.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
